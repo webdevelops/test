7b6c748980ec5f22e9c38e08f061da44760f6f71
oleg <oleg1ostrikov@gmail.com>
2020-02-17 20:25:43 +0200

    Updates

---
+++ precache-manifest.d94ef1172c541e2a96bf2de00a335902.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2bd3b5e4cf89b1f382ae1a22f7b5c4",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "100cb4efd59887d82934",
+    "url": "/test/static/css/2.60d173ac.chunk.css"
+  },
+  {
+    "revision": "100cb4efd59887d82934",
+    "url": "/test/static/js/2.f0caa990.chunk.js"
+  },
+  {
+    "revision": "ffb5b19324833cd4d400993df1934573",
+    "url": "/test/static/js/2.f0caa990.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c9c4e51c7b1c024ad8a",
+    "url": "/test/static/js/main.03c66b16.chunk.js"
+  },
+  {
+    "revision": "25efeaacbead4a84b640",
+    "url": "/test/static/js/runtime-main.909a2939.js"
+  }
+]);
