1e6f468b42b6e1aa2613905ff6c8f2cfd7366d61
oleg <oleg1ostrikov@gmail.com>
2020-03-26 18:41:43 +0200

    testing heroku

--- mern/mern-1/app.js
+++ mern/mern-1/app.js
@@ -11,12 +11,11 @@ app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/link', require('./routes/link.routes'));
 app.use('/t', require('./routes/redirect.routes'));
 
-if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    // res.sendFile(path.json(__dirname, '../client', 'build', 'index.html'));
   });
 }
 
