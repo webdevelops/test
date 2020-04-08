8bd6741bbbecdf3d86a1c4f77f87bff0b7c5668d
oleg <oleg1ostrikov@gmail.com>
2020-04-08 22:55:01 +0300

    min

---
+++ jsTasks/cycle/cycle.js
@@ -0,0 +1,26 @@
+for (let i = 0; i < 10; i++) {
+  if (i % 2 == 0) continue;
+
+  console.log(i);
+}
+
+console.log('----------');
+
+for (let i = 0; i <= 10; i++) {
+  if (i % 2 == 0 && i != 0) {
+    console.log(i);
+  }
+}
+
+console.log('----------');
+
+let i = 0;
+while (i < 3) {
+  console.log('number: ', i);
+  i++;
+}
+
+console.log('----------');
+
+
+
--- jsTasks/logicalOperators/login.js
+++
@@ -1,9 +0,0 @@
-// const login = prompt('Enter your Login', '');
-// const login = 'Admin';
-const login = '';
-
-if (login === '' || login === null) {
-  // alert("Cancel");
-  console.log("Cancel");
-}
-// console.log("login", login)
