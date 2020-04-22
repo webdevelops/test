861558ad83ba618983a4e0372f2fab64f0123cd1
oleg <oleg1ostrikov@gmail.com>
2020-04-22 19:52:30 +0300

    testing task planing

---
+++ jsTasks/timer/printNumbers.js
@@ -0,0 +1,24 @@
+function printNumbers(from, to) {
+  let timerId = setInterval(() => {
+    console.log(from);
+    
+    if (from == to) clearInterval(timerId);
+    from++;
+
+  }, 1000);
+}
+
+function printNumbers2(from, to) {
+  setTimeout(function nextNumber() {
+    console.log(from);
+
+    if (from < to) {
+      setTimeout(nextNumber, 1000);
+    } 
+    from++;
+
+  }, 1000);
+}
+
+printNumbers(1, 10);
+printNumbers2(1, 10);
