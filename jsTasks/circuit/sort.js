7524eb841bedba01f3b76f1ae184f63f3eb59843
oleg <oleg1ostrikov@gmail.com>
2020-05-02 15:17:23 +0300

    revised method sort

---
+++ jsTasks/circuit/sort.js
@@ -0,0 +1,18 @@
+let users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" }
+];
+
+function byField(field) {
+  return (a, b) => (a[field] > b[field]) ? 1 : -1;
+}
+
+users.sort(byField('name'));
+console.log(users);
+
+users.sort(byField('surname'));
+console.log(users);
+
+users.sort(byField('age'));
+console.log(users);
