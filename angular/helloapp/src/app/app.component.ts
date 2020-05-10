3b947525625e1087232bdf80d9406e11e0c85b62
oleg <oleg1ostrikov@gmail.com>
2020-05-10 13:58:48 +0300

    testing styleUrls

--- angular/helloapp/src/app/app.component.ts
+++ angular/helloapp/src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<label>Enter your name:</label>
-    <input [(ngModel)]="name" placeholder="name">
-    <h1>Welcome {{name}}!</h1>`
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   name = '';
