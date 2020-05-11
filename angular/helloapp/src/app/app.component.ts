b545e2310b2a9c8523e9b18d9dbbcdccbeb50a9b
oleg <oleg1ostrikov@gmail.com>
2020-05-11 14:12:03 +0300

    testing bindings

--- angular/helloapp/src/app/app.component.ts
+++ angular/helloapp/src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = '';
+  name: string = 'Oleg';
+  age: number = 45;
+  colspan: number = 2;
+
+  count: number = 0;
+  increase($event: any): void {
+    this.count++;
+    console.log($event);
+  }
+
+  isRed: boolean = false;
+  blue: string = "isbluebox";
+  green: string = "isgreenbox"
 }
