73192e838a98ee790069f9e6966b62dbd02ff262
oleg <oleg1ostrikov@gmail.com>
2020-05-15 16:16:26 +0300

    testing bcryptjs @ express-validator

--- mern/mern-3/routes/auth.routes.js
+++ mern/mern-3/routes/auth.routes.js
@@ -1,24 +1,49 @@
 const { Router } = require('express');
-const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
 const router = Router();
+const User = require('../models/User');
 
 //  /api/auth/register
-routes.post('/register', async (req, res) => {
-  try {
-    const { email, password } = req.body;
+router.post(
+  '/register',
+  [
+    check('email', 'Wrong email.').isEmail(),
+    check('password', 'At last 6 characters.').isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-    const candidate = await User.findOne({ email });
-    if (candidate) {
-      return res.status(400).json({ message: 'Such user already exists' }); // 400 - Bad Request
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Unccorect registration data.'
+        })
+      }
+
+      const { email, password } = req.body;
+      const candidate = await User.findOne({ email });
+
+      if (candidate) {
+        return res.status(400).json({ message: 'Such user already exists' }); // 400 - Bad Request
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ email, password: hashedPassword });
+
+      await user.save();
+
+      res.status(201).json({ message: 'User created.' });
+
+    } catch (err) {
+      res.status(500).json({ message: 'Somethin went wrong, try again.' }); // 500 - Server Error
     }
-
-  } catch (err) {
-    res.status(500).json({ message: 'Somethin went wrong, try again.' }); // 500 - Server Error
-  }
-});
+  });
 
 //  /api/auth/login
-routes.post('/login', async (req, res) => {
+router.post('/login', async (req, res) => {
 
 });
 
