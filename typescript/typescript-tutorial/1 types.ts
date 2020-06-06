d37d94afa9f738dd2e29cbef506a672ce92c6c02
oleg <oleg1ostrikov@gmail.com>
2020-06-06 13:01:03 +0300

    types

---
+++ typescript/typescript-tutorial/1 types.ts
@@ -0,0 +1,9 @@
+const isFetched: boolean = true;
+const isLoading: boolean = false;
+
+const int: number = 45;
+const float: number = 2.5;
+const num: number = 3e10;
+
+const message: string = 'Hello TypeScript';
+
