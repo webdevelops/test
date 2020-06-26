14c8f25ecbae7d9ed444e36e11ef85cc82b41f6d
oleg <oleg1ostrikov@gmail.com>
2020-06-26 14:52:03 +0300

    pipes

--- angular/tutorial/src/app/app.component.ts
+++ angular/tutorial/src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Post {
+  title: string,
+  text: string
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isVisible = true;
+  search = '';
+  searchField = 'title';
+  posts: Post[] = [
+    { title: 'Bear', text: 'The best drink in the world' },
+    { title: 'JavaScript', text: 'The best language in the world' }
+  ];
+
+  p: Promise<string> = new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve('Promise resolved');
+    }, 4000)
+  });
+
+  date: Observable<Date> = new Observable(obs => {
+    setInterval(() => {
+      obs.next(new Date());
+    }, 1000);
+  })
+
+  addPost() {
+    this.posts.unshift({
+      title: 'Angular 9',
+      text: 'Vlad'
+    });
+    console.log(this.posts)
+  };
 }
--- angular/tutorial/src/app/app.module.ts
+++ angular/tutorial/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { StyleDirective } from './directives/style.directive';
-import { IfnotDirective } from './directives/ifnot.directive';
+import { MultByPipe } from './pipes/mult-by.pipe';
+import { ExMarksPipe } from './pipes/ex-marks.pipe';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StyleDirective,
-    IfnotDirective,
+    MultByPipe,
+    ExMarksPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
