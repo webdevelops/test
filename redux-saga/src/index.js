72e702d7320884919a611363489b4551d5013fa7
oleg <oleg1ostrikov@gmail.com>
2020-07-02 10:30:04 +0300

    store

--- redux-saga/src/index.js
+++ redux-saga/src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+import { createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+
 import * as serviceWorker from './serviceWorker';
+import { rootReducer } from './redux/rootReducer';
+import App from './App';
+
+// const store = createStore(rootReducer, compose(
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// ));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
+const store = createStore(rootReducer, composeEnhancers());
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
 
 render(
   <React.StrictMode>
-    <App />
+    {app}
   </React.StrictMode>,
   document.getElementById('root')
 );
