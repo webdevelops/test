2db69e6f57883c89d59bc7980fcb877a358a4b97
oleg <oleg1ostrikov@gmail.com>
2020-07-08 22:51:07 +0300

    auth service

--- angular/angular-blog/src/app/admin/admin.module.ts
+++ angular/angular-blog/src/app/admin/admin.module.ts
@@ -8,6 +8,8 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
+import { AuthService } from './shared/services/auth.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { EditPageComponent } from './edit-page/edit-page.component';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     RouterModule.forChild([
       {
         path: '', component: AdminLayoutComponent, children: [
@@ -33,7 +36,8 @@ import { EditPageComponent } from './edit-page/edit-page.component';
       }
     ])
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthService]
 })
 export class AdminModule {
 
--- angular/angular-blog/src/app/admin/login-page/login-page.component.ts
+++ angular/angular-blog/src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { User } from "../../shared/interfaces";
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -10,12 +14,38 @@ export class LoginPageComponent implements OnInit {
 
   form: FormGroup
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl(),
-      password: new FormControl()
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ]),
+    })
+  }
+
+  submit() {
+    console.log(this.form);
+    if (this.form.invalid) {
+      return;
+    }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    }
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset();
+      this.router.navigate(['/admin', 'dashboard']);
     })
   }
 
