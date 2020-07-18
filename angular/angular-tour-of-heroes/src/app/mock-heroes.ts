1c4c5e136ed8e2ace72782aa56f22e671082cd76
oleg <oleg1ostrikov@gmail.com>
2020-07-18 22:14:00 +0300

    binding

---
+++ angular/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 11, name: 'Celeritas' },
+  { id: 11, name: 'Magneta' },
+  { id: 11, name: 'RubberMan' },
+  { id: 11, name: 'Dyname' },
+  { id: 11, name: 'Dr IQ' },
+  { id: 11, name: 'Magma' },
+  { id: 11, name: 'Tornado' }
+];
