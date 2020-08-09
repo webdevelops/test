1fd4bd7b6b12743c163cf1f8315ece28cbaa31c9
Oleg_remote <olos1olos@gmail.com>
2020-08-09 12:48:37 +0300

    server2

--- angular/mean/backend/app.js
+++ angular/mean/backend/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {id: "123qwe", title: "First server-side post", content: "This is coming from the server"},
+    {id: "234wer", title: "Second server-side post", content: "This is coming from the server"},
+  ];
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+  res.status(200).json({
+    message: "Posts fetched succesfully!!!",
+    posts: posts
+  })
 });
 
 module.exports = app;
