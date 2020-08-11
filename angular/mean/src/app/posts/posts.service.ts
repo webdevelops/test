eff1be08b479285dc8af07256de67bf1deeaf910
Oleg_remote <oleg1ostrikov@gmail.com>
2020-08-11 20:02:24 +0300

    save post

--- angular/mean/src/app/posts/posts.service.ts
+++ angular/mean/src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -16,12 +17,20 @@ export class PostsService {
   ) { }
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe(postData => {
-        this.posts = postData.posts;
+    this.http
+      .get<{ message: string, posts: any }>('http://localhost:3000/api/posts')
+      .pipe(map(postData => {
+        return postData.posts.map(post => {
+          return {
+            ...post,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe(transformsPost => {
+        this.posts = transformsPost;
         this.postsUpdated.next([...this.posts]);
-
-      });
+      })
   }
 
   getPostUpdateListener() {
@@ -30,7 +39,7 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    this.http.post<{ message: string }>('http://localhost:3000/api/posts', post)
       .subscribe(() => {
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
