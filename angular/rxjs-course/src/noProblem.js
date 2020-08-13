d3a3045d2d2f42dc5cb6b6d1eafd00dbf5e20e01
Oleg_remote <oleg1ostrikov@gmail.com>
2020-08-13 15:08:14 +0300

    take, scak

---
+++ angular/rxjs-course/src/noProblem.js
@@ -0,0 +1,13 @@
+// const btn = document.getElementById('int');
+// const rxBtn = document.getElementById('rx');
+
+// const other = [
+//   { name: 'Vladilen', age: 25 },
+//   { name: 'Elena', age: 17 },
+//   { name: 'Ivan', age: 18 },
+//   { name: 'Igor', age: 14 },
+//   { name: 'Lisa', age: 32 },
+//   { name: 'Irina', age: 23 },
+//   { name: 'Oleg', age: 20 }
+// ];
+
