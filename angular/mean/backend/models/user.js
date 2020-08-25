fae628b32883ed060a46a6b713b08bb7f60a5ea8
Oleg_remote <oleg1ostrikov@gmail.com>
2020-08-25 14:35:17 +0300

    signup

--- angular/mean/backend/models/user.js
+++ angular/mean/backend/models/user.js
@@ -6,6 +6,6 @@ const userSchema = mongoose.Schema({
   password: { type: String, required: true }
 });
 
-userSchema.plagin(uniqueValidator);
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
--- angular/mean/src/app/auth/signup/signup.component.ts
+++ angular/mean/src/app/auth/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { AuthService } from '../auth.service';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -9,13 +11,19 @@ import { NgForm } from '@angular/forms';
 export class SignupComponent implements OnInit {
   isLoading = false;
 
-  constructor() { }
+  constructor(
+    public authService: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
 
   onSignup(form: NgForm) {
-    console.log('form', form.value);
+    if (form.invalid) {
+      return;
+    }
+    
+    this.authService.createUser(form.value.email, form.value.password);
   }
 
 }
