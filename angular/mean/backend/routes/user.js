d14cfb3c58c769fbaa6c9937eadf7af82ef63c8c
oleg <oleg1ostrikov@gmail.com>
2020-08-25 16:59:09 +0300

    sending token

--- angular/mean/backend/routes/user.js
+++ angular/mean/backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 const user = require('../models/user');
@@ -27,5 +28,41 @@ router.post('/signup', (req, res, next) => {
         });
     });
 });
+
+router.post('/login', (req, res, next) => {
+  let fetchedUser;
+
+  User
+    .findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: 'Auth failed'
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        return res.status(401).json({
+          message: 'Auth failed'
+        })
+      }
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        "sekret_should_be_longer",
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({
+        token: token
+      });
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: 'Auth failed'
+      })
+    });
+})
 
 module.exports = router;
