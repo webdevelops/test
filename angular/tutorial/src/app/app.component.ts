4cd45fd2c7e4554211e0b8baf7d2cd3858d0eb4f
Oleg_remote <oleg1ostrikov@gmail.com>
2020-08-28 22:28:05 +0300

    HostListener

--- angular/tutorial/src/app/app.component.ts
+++ angular/tutorial/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core'
 export class AppComponent {
   currentItem = 'Hello';
   items = ['item1', 'item2', 'item3', 'item4'];
+  color = 'yellow';
 
   addItem(newItem: string) {
     this.items.push(newItem);
--- angular/tutorial/src/app/fundamentals/highlight.directive.ts
+++ angular/tutorial/src/app/fundamentals/highlight.directive.ts
@@ -1,12 +1,21 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+  @Input('appHighlight') highlightColor: string;
 
-  constructor(el: ElementRef) {
-    el.nativeElement.style.backgroundColor = 'yellow';
+  constructor(private el: ElementRef) {}
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.highlightColor || 'red');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
   }
 
+  highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 }
