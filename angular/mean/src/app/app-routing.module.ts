7e476a02a91e5e7d0995fe1108d042716f761426
Oleg_remote <oleg1ostrikov@gmail.com>
2020-09-01 11:09:18 +0300

    lazy loading

--- angular/mean/src/app/app-routing.module.ts
+++ angular/mean/src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AuthGuard } from './auth/auth.guard';
 
 
@@ -12,15 +10,14 @@ const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'create', component: PostCreateComponent, canActivate: [AuthGuard] },
   { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
-];
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: [AuthGuard]
-})
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+    providers: [AuthGuard]
+  })
 export class AppRoutingModule {
 
 }
--- angular/mean/src/app/auth/auth.module.ts
+++ angular/mean/src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { SignupComponent } from './signup/signup.component';
     SignupComponent,
   ],
   imports: [
-    FormsModule
+    FormsModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule {}
