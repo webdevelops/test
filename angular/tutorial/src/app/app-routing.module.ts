877eafb401d53e1327ee2067d33aef0c177f13ac
oleg <oleg1ostrikov@gmail.com>
2020-09-07 22:18:15 +0300

    app-routing.module

--- angular/tutorial/src/app/app-routing.module.ts
+++ angular/tutorial/src/app/app-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './home-page/home-page.component'
 import { SvgComponent } from './fundamentals/svg/svg.component';
-import { FirstComponent } from './fundamentals/routing/first/first.component';
-import { SecondComponent } from './fundamentals/routing/second/second.component';
 
 const routes: Routes = [
-  // {path: '', component: FirstComponent},
-  // {path: 'second', component: SecondComponent},
-
   { path: '', component: HomePageComponent, pathMatch: 'full' },
   { path: 'svg', component: SvgComponent },
   { path: 'about', loadChildren: () => import('./about-page/about-page.module').then(m => m.AboutPageModule) }
