06275cddeab4c9e7e8b99966b97ccd66c4c5b264
oleg <oleg1ostrikov@gmail.com>
2020-09-13 23:16:35 +0300

    SidenavModule

--- testTasks/2020/testAngular/src/app/app.module.ts
+++ testTasks/2020/testAngular/src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { JumbotronComponent } from './home-page/jumbotron/jumbotron.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    JumbotronComponent
+    JumbotronComponent,
+    ToolBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ testTasks/2020/testAngular/src/app/tool-bar/tool-bar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-tool-bar',
+  templateUrl: './tool-bar.component.html',
+  styleUrls: ['./tool-bar.component.scss']
+})
+export class ToolBarComponent implements OnInit, OnDestroy {
+  mobileQuery: MediaQueryList;
+  fillerContent = Array.from({ length: 50 }, (_, i) => `Nav Item ${i + 1}`);
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+}
