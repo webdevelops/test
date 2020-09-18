b55499caabb31378961fe57fb81e808e499fe760
oleg <oleg1ostrikov@gmail.com>
2020-09-18 11:39:18 +0300

    guards

--- angular/tutorial/src/app/app-routing.module.ts
+++ angular/tutorial/src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { SvgComponent } from './fundamentals/svg/svg.component';
 import { HeroesComponent } from './fundamentals/dependency/heroes/heroes.component';
 import { HeroComponent } from './fundamentals/dependency/heroes/hero/hero.component';
+import { YourGuardGuard } from './fundamentals/routing/your-guard.guard';
 
 const routes: Routes = [
   { path: 'svg', component: SvgComponent },
   { path: 'heroes', component: HeroesComponent },
-  { path: 'heroes/:heroId', component: HeroComponent },
+  { path: 'heroes/:heroId', component: HeroComponent, canActivate: [YourGuardGuard] },
   { path: 'about', loadChildren: () => import('./about-page/about-page.module').then(m => m.AboutPageModule) },
   { path: '', component: HomePageComponent, pathMatch: 'full' },
   { path: '**', redirectTo: '', pathMatch: 'full' }
