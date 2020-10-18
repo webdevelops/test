e51bbbf0211fbf85e1cc0b66732103e488c8ac05
oleg1ostrikov@gmail.com <oleg1ostrikov@gmail.com>
2020-10-18 10:18:02 +0300

    profil

--- testTasks/realwords/rw1/src/app/profile/profile-articles/profile-articles.component.ts
+++ testTasks/realwords/rw1/src/app/profile/profile-articles/profile-articles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ArticleListConfig, Profile } from 'src/app/core';
 
 @Component({
   selector: 'app-profile-articles',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileArticlesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  profile: Profile;
+  articleConfig: ArticleListConfig = {
+    type: 'all',
+    filters: {}
+  };
 
   ngOnInit() {
+    this.route.parent.data.subscribe(
+      (data: { profile: Profile }) => {
+        this.profile = data.profile;
+        this.articleConfig = {
+          type: 'all',
+          filters: {}
+        };
+        this.articleConfig.filters.author = this.profile.username;
+      }
+    )
   }
 
 }
--- testTasks/realwords/rw1/src/app/profile/profile-fivorites/profile-fivorites.component.ts
+++ testTasks/realwords/rw1/src/app/profile/profile-fivorites/profile-fivorites.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArticleListConfig, Profile } from 'src/app/core';
 
 @Component({
   selector: 'app-profile-fivorites',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-fivorites.component.scss']
 })
 export class ProfileFivoritesComponent implements OnInit {
+  profile: Profile;
+  favoritesConfig: ArticleListConfig = {
+    type: 'all',
+    filters: {}
+  };
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.route.parent.data.subscribe(
+      (data: { profile: Profile }) => {
+        this.profile = data.profile;
+        this.favoritesConfig.filters.favorited = this.profile.username
+      }
+    )
   }
 
 }
