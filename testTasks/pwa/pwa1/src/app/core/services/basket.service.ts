185f59db8740baffd563a90238d715e435fffead
oleg1ostrikov@gmail.com <oleg1ostrikov@gmail.com>
2020-11-04 14:31:55 +0200

    basket service

--- testTasks/pwa/pwa1/src/app/core/services/basket.service.ts
+++ testTasks/pwa/pwa1/src/app/core/services/basket.service.ts
@@ -4,6 +4,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 
 // Libs
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 // App
 import { ProductModel } from '../models/product.model';
@@ -15,18 +16,28 @@ export class BasketService {
   constructor(private firestore: AngularFirestore) { }
 
   // addToCart(product: ProductModel): Observable<ProductModel> {
-  //     return this.firestore
-  //       .collection('basket')
-  //       // .doc(`${product.productId}`)
-  //       .doc(`pro-basket`)
-  //       .set(product)
-  //       .then(product => of(product))
-  //       .catch(error => console.log('Error: ', error))
-  //   }
+  //   return this.firestore
+  //     .collection('basket')
+  //     .doc(`${product.productId}`)
+  //     .set(product)
+  //     .then(product => of(product))
+  //     .catch(error => console.log('Error: ', error))
+  // }
 
   addToCart(product: ProductModel): Observable<boolean> {
     return of(true);
   }
+
+  // ----------------------------------
+
+  // removeFromCart(productId) {
+  //   return this.firestore
+  //     .collection('basket')
+  //     .doc('productId')
+  //     .delete()
+  //     .then(() => of(true))
+  //     .catch(error => console.log(error))
+  // }
 
   removeFromCart(productId: string): Observable<boolean> {
     return of(true);
