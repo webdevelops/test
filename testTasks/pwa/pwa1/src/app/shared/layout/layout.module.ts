97d202e2e3f3bafc88c09b84516b05710cd1deb0
oleg <oleg1ostrikov@gmail.com>
2020-12-21 19:44:02 +0200

    m0

--- testTasks/pwa/pwa1/src/app/shared/layout/layout.module.ts
+++ testTasks/pwa/pwa1/src/app/shared/layout/layout.module.ts
@@ -38,7 +38,7 @@ import { HomePageComponent } from '../../modules/home-page/home-page.component';
     FooterComponent,
     ProductListComponent,
     GreetingSectionComponent,
-    // temp-11
+    // temp-11-0
   ]
 })
 export class LayoutModule { }
