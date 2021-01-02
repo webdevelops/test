38bb6ea2cb18ba67feb4d885a75dd173bcd82ce9
oleg <oleg1ostrikov@gmail.com>
2021-01-02 19:02:55 +0200

    ol11 task 1

--- testTasks/pwa/pwa1/src/app/shared/layout/layout.module.ts
+++ testTasks/pwa/pwa1/src/app/shared/layout/layout.module.ts
@@ -39,7 +39,7 @@ import { HomePageComponent } from '../../modules/home-page/home-page.component';
     ProductListComponent,
     GreetingSectionComponent,
     // master - 4
-    // ol-9  - task 2
+    // ol-11  - task 1
   ]
 })
 export class LayoutModule { }
