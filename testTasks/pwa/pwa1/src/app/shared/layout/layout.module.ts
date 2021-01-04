341d8ffcd33cfae0dcd50b92a08b1c1ffd52cd67
oleg <oleg1ostrikov@gmail.com>
2021-01-04 23:36:43 +0200

    merge

--- testTasks/pwa/pwa1/src/app/shared/layout/layout.module.ts
+++ testTasks/pwa/pwa1/src/app/shared/layout/layout.module.ts
@@ -38,7 +38,7 @@ import { HomePageComponent } from '../../modules/home-page/home-page.component';
     FooterComponent,
     ProductListComponent,
     GreetingSectionComponent,
-    // ol-13  - task 1
+    // ol-14  - task 1
   ]
 })
 export class LayoutModule { }
