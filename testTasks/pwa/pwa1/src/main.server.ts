0e414dea3922cd48f2b0166129537309c5863739
oleg <oleg1ostrikov@gmail.com>
2021-01-30 23:02:34 +0200

    git reset --soft HEAD^ - 6

--- testTasks/pwa/pwa1/src/main.server.ts
+++ testTasks/pwa/pwa1/src/main.server.ts
@@ -8,3 +8,6 @@ if (environment.production) {
 
 export { AppServerModule } from './app/app.server.module';
 export { renderModule, renderModuleFactory } from '@angular/platform-server';
+
+// test - 2
+// git reset --soft HEAD^ - 1 from main.server
--- testTasks/pwa/pwa1/src/main.ts
+++ testTasks/pwa/pwa1/src/main.ts
@@ -11,3 +11,12 @@ document.addEventListener('DOMContentLoaded', () => {
   platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 });
+
+// test-1
+// git reset --soft HEAD^
+// git reset --soft HEAD^ - 2
+// git reset --soft HEAD^ - 3
+// git reset --soft HEAD^ - 4
+// git reset --soft HEAD^ - 4.5
+// git reset --soft HEAD^ - 5
+// git reset --soft HEAD^ - 6
